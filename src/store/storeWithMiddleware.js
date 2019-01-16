f2c9c180ef55925bdb64e89d381d69d99aa34825
goingta <tanglei@doctorwork.com>
2019-01-16 11:25:21 +0800

    thunk

--- src/store/storeWithMiddleware.js
+++ src/store/storeWithMiddleware.js
@@ -2,8 +2,11 @@ import { createStore, applyMiddleware } from "../redux/redux";
 import counter from "../reducers/counter";
 import todo from "../reducers/todo";
 import combineReducers from "../redux/combineReducers";
-
-// 这种拿不到store，而且只能在各个业务逻辑里面做
+const INCREMENT = "INCREMENT";
+/**
+ *
+ * 最外层的是一个新的dispatch，所以action是外面传过来的
+ */
 const logger = store => next => action => {
   console.log("before", store.getState());
   console.log("action", action);
@@ -11,8 +14,23 @@ const logger = store => next => action => {
   console.log("after", store.getState());
 };
 
+const thunk = store => next => action => {
+  if (typeof action === "function") {
+    action(next);
+  } else {
+    next(action);
+  }
+};
+
 const reducer = combineReducers({ counter, todo });
 
-const store = applyMiddleware(logger)(createStore)(reducer);
+// const store = applyMiddleware(logger)(createStore)(reducer);
+const store = applyMiddleware(thunk)(createStore)(reducer);
+
+store.dispatch(dispatch => {
+  setTimeout(() => {
+    dispatch({ type: INCREMENT, amount: 3 });
+  }, 3000);
+});
 
 export default store;
